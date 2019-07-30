d1bea45cfce7ec5de3d223bc0c8ab33159b88060
deepak-kumar-bg <deepak.kumar@bigsteptech.com>
2019-07-30 13:18:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$conn=mysqli_connect("localhost","root","bigstep","demo");
+if (! $conn ) {
+	die('Could not connect: ' . mysql_error());
+}
+$sql="SELECT * FROM user WHERE name='deepak'";
+
+$result= mysqli_query($conn,$sql);
+if(empty($result)){
+	echo "worng";
+}
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["user_id"]. " - Name: " . $row["name"]. " -address: " . $row["address"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
